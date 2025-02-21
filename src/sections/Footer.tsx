7fec2bd990425c6d9b8043b73768547dc5cf695d
Anky9779 <ankitwaikar9779@gmail.com>
2025-02-21 12:20:35 -0500

    Initial Commit

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -3,7 +3,7 @@ import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 const footerLinks = [
   {
     title: "YouTube",
-    href: "#",
+    href: "https://www.youtube.com",
   },
   {
     title: "Twitter",
@@ -27,7 +27,7 @@ export const Footer = () => {
           <div className="text-white/40 ">&copy; 2024. All rights reserved.</div>
           <nav className="flex flex-col md:flex-row items-center gap-8">
             {footerLinks.map((link) => (
-              <a href="#" key={link.title} className="inline-flex items-center gap-1.5">
+              <a href={link.href} key={link.title} className="inline-flex items-center gap-1.5">
                 <span className="font-semibold"> {link.title} </span>
                 <ArrowUpRightIcon className="size-4"/>
               </a>
