588c1ff3dbea373232c5978031be0f3d2e6fb81d
Anky9779 <ankitwaikar9779@gmail.com>
2025-03-01 12:49:53 -0500

    upd metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,8 @@ const calistoga = Calistoga({
 });
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
-  description: "Created with the help of Frontend Tribe",
+  title: "Ankit Waikar's Portfolio",
+  description: "Business Analyst | Data Analyst | Product Manager",
 };
 
 export default function RootLayout({
