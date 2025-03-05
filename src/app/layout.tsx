fd7f448e479f114413556180a998116b39c93c59
Anky9779 <ankitwaikar9779@gmail.com>
2025-03-05 15:38:49 -0500

    Tag Added G-code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,16 @@
 import type { Metadata } from "next";
-import {Inter, Calistoga } from 'next/font/google';
+import { Inter, Calistoga } from 'next/font/google';
 import './globals.css';
 import { twMerge } from "tailwind-merge";
 import { Analytics } from "@vercel/analytics/react";  
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import Script from 'next/script'; // Import next/script
 
-const inter = Inter({subsets: ['latin'],variable: "--font-sans"})
+const inter = Inter({ subsets: ['latin'], variable: "--font-sans" });
 const calistoga = Calistoga({
   subsets: ['latin'],
   variable: "--font-serif",
-  weight:["400"],
+  weight: ["400"],
 });
 
 export const metadata: Metadata = {
@@ -24,6 +25,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager script */}
+        <Script 
+          async 
+          src="https://www.googletagmanager.com/gtag/js?id=G-N2S4RYH6KJ"
+          strategy="afterInteractive" // Ensures it loads after the page is interactive
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-N2S4RYH6KJ');
+          `}
+        </Script>
+      </head>
       <body 
         className={twMerge(
           inter.variable, 
